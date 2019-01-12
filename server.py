08e4980eb2b16cf289674af37d8757df464822bb
Anna Fodor <fodor.ak@gmail.com>
2019-01-12 19:23:30 +0100

    add favicon

--- server.py
+++ server.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/favicon.ico')
+def fav():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
